5fa9a4831daf86a247f33e74a8f4fca1aab07c79
Florian Gröne <florian@andbam.co>
2021-04-11 17:48:21 +0200

    Implemented changes from Sam Atkinson (Thank you soooo much for your helpgit add index.html)

--- js/mySearchFilter.js
+++ js/mySearchFilter.js
@@ -1,39 +1,14 @@
-// ====================================================================
-// Try #1: Using the geeksforgeeks.org approach: 
-// https://www.geeksforgeeks.org/search-bar-using-html-css-and-javascript/
-// Failed due to Uncaught TypeError: Cannot read property 'includes' of undefined in line 10
-// I sadly don't understand how about 50% of this works and I don't have enough time to go in and find out about every single detail.
-
-// function search_photo() {
-//     let input = document.getElementById('searchbar').value
-//     input=input.toLowerCase();
-//     let x = document.getElementsByTagName('A')[0].getAttribute('data-caption');
-
-//     for (let i = 0; i < x.length; i++) {
-//         if (!x[i].innerHTML.includes(input)) {
-//             x[i].style.display="none";
-//         } else {
-//             x[i].style.display="list-item";
-//         }
-//     }
-// }
-// For re-activation: Add "onkeyup="search_photo()"" to the <input>
-
-// ====================================================================
-// Try #2: Using The approach from Amber: 
-// https://treehouse-fewd-102.slack.com/archives/CBPTF1WJK/p1616695567056100
-// Also doesn't work :/
-
-
-const search = document.querySelector('#searchbar');
-const boxTexts = document.getElementsByTagName("a")[0].getAttribute("data-caption");
-
+const search = document.getElementById('searchbar');
+const boxTexts = document.querySelectorAll("a");
 const handleSearch = event => {
   const searchTerm = event.target.value.toLowerCase();
-  
+  console.log(searchTerm);
   boxTexts.forEach(boxText => {
-    const text = boxText.textContent.toLowerCase();
-    const box = boxText.parentElement;
+    //get the value text of the attribute for the a elements (const boxTexts = document.querySelectorAll("a");)
+    const text = boxText.getAttribute('data-caption');
+    //variable that holds the a element that has data-caption text that is in the search 
+    const box = boxText;
+
     
    // if(text.indexOf(searchTerm) > -1) {
     if(text.includes(searchTerm)) {
@@ -42,9 +17,8 @@ const handleSearch = event => {
       box.style.display = "none";  
     }
   });
-
 };
 
-search.addEventListener('keyup', handleSearch);
+// search.addEventListener('keyup', handleSearch);
 
-// :'(
+// Thanks to @Sam_Atkinson. Basically all credit goes to her for figuring this out.
